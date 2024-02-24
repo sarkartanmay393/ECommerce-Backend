db3663121a98ceed8f542a5d4082db0f9770887f
Tanmay Sarkar <sarkartanmay393@gmail.com>
2024-02-24 20:45:09 +0530

    feat: aiven psql setup with migration

--- utils/database.ts
+++ utils/database.ts
@@ -1,8 +1,20 @@
 import { Sequelize } from "sequelize";
+import fs from "fs";
 
-export const sequelize = new Sequelize({
-  host: "localhost",
-  database: "triveous",
-  username: "postgres",
-  dialect: "postgres",
-});
+export const sequelize = new Sequelize(
+  {
+    username: 'avnadmin',
+    password: '',
+    database: 'triveous',
+    host: 'pg-1d02b618-sarkartanmay393-034b.a.aivencloud.com',
+    port: 14056,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(__dirname + "/../ca.pem").toString(),
+      },
+    },
+  }
+);
